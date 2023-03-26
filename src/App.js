8131fa1bcdadf809089670169ac62cefcb5c9576
Patryk Vincerre <bazior.patryk@gmail.com>
2023-03-26 20:06:16 +0200

    timer-app

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import Container from "./components/Container/Container";
+import Timer from "./components/Timer/Timer";
+import Button from "./components/Button/Button";
+import { useState, useEffect } from "react";
+import styles from "./App.module.scss";
+
+const App = (props) => {
+  const [time, setTime] = useState(0);
+  const [timer, setTimer] = useState("");
+
+  const start = () => {
+    setTimer(
+      setInterval(() => {
+        setTime((preValue) => preValue + 1);
+      }, 1)
+    );
+  };
+
+  const stop = () => {
+    clearInterval(timer);
+  };
+
+  const reset = () => {
+    setTime(0);
+  };
+
+  useEffect(() => {
+    return () => {
+      if (timer) clearInterval(timer);
+    };
+  }, [timer]);
+
+  return (
+    <div className={styles.timer_wrapper}>
+      <Timer time={time} />
+      <Container>
+        <Button onClick={start}>Start</Button>
+        <Button onClick={stop}>Stop</Button>
+        <Button onClick={reset}>Reset</Button>
+      </Container>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Timer/Timer.js
@@ -0,0 +1,21 @@
+import styles from "./Timer.module.scss";
+
+const Timer = ({ time }) => {
+  const formatTime = (milliseconds) => {
+    let hours = Math.floor((milliseconds / (1000 * 60 * 60)) % 24);
+    let minutes = Math.floor((milliseconds / (1000 * 60)) % 60);
+    let seconds = Math.floor((milliseconds / 1000) % 60);
+    let miliseconds = Math.floor(milliseconds % 1000);
+
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+    miliseconds = String(miliseconds).padStart(1, "0");
+
+    return `${hours}:${minutes}:${seconds}.${miliseconds}`;
+  };
+
+  return <div className={styles.formatter}>{formatTime(time)}</div>;
+};
+
+export default Timer;
